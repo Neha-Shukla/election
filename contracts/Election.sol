17276c3f3f28a3cf60ea602970547f94bdf0b1e9
Neha-shukla <nshukl23@gmail.com>
2020-05-03 10:55:26 +0530

    adding voting feature

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.21 <0.7.0;
+
 contract Election{
     //model a candidate
     struct Candidate{
@@ -6,7 +7,8 @@ contract Election{
         string name;
         uint voteCount;
     }
-
+    //store voters
+    mapping(address => bool) public voters;
     //store candidate
     //fetch candidate
     mapping(uint => Candidate) public candidates;
@@ -19,5 +21,15 @@ contract Election{
     function addCandidate(string memory _name ) private{
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+    }
+
+    function vote(uint _candidateId) public{
+        // require that they haven't voted before
+        require(!voters[msg.sender], "already voted");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "not a valid candidate");
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
